9cad918ff7176d16243735be5728ae70a15b5e76
Takeshi Kishi <takeyaqa@gmail.com>
2026-04-16 19:57:34 +0900

    test: add snapbox and implement CLI tests

---
+++ tests/cli.rs
@@ -0,0 +1,77 @@
+use snapbox::cmd::{Command, cargo_bin};
+
+#[test]
+fn set_command_parses_namespace_and_env_name() {
+    Command::new(cargo_bin("divechain"))
+        .args(["set", "aws", "AWS_ACCESS_KEY_ID"])
+        .assert()
+        .success();
+}
+
+#[test]
+fn list_command_prints_namespaces() {
+    Command::new(cargo_bin("divechain"))
+        .arg("list")
+        .assert()
+        .stdout_eq("aws\n")
+        .success();
+}
+
+#[test]
+fn exec_command_parses_namespace_and_command() {
+    Command::new(cargo_bin("divechain"))
+        .args(["exec", "aws", "printenv", "AWS_ACCESS_KEY_ID"])
+        .assert()
+        .success();
+}
+
+#[test]
+fn help_lists_supported_subcommands() {
+    Command::new(cargo_bin("divechain"))
+        .arg("help")
+        .assert()
+        .stdout_eq(
+            r"Usage: divechain <COMMAND>
+
+Commands:
+  list  
+  set   
+  exec  
+  help  Print this message or the help of the given subcommand(s)
+
+Options:
+  -h, --help     Print help
+  -V, --version  Print version
+",
+        )
+        .success();
+}
+
+#[test]
+fn rejects_empty_invocation() {
+    Command::new(cargo_bin("divechain")).assert().failure();
+}
+
+#[test]
+fn rejects_unknown_command() {
+    Command::new(cargo_bin("divechain"))
+        .arg("unknown")
+        .assert()
+        .failure();
+}
+
+#[test]
+fn rejects_missing_env_for_set() {
+    Command::new(cargo_bin("divechain"))
+        .args(["set", "aws"])
+        .assert()
+        .failure();
+}
+
+#[test]
+fn rejects_missing_command_for_exec() {
+    Command::new(cargo_bin("divechain"))
+        .args(["exec", "aws"])
+        .assert()
+        .failure();
+}
